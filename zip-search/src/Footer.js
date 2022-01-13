8de39785f6c8ca20903bd308e105bd5e006405ae
Zixin Zou <zxinmm@gmail.com>
2022-01-13 14:41:36 -0500

    comment

--- zip-search/src/Footer.js
+++ zip-search/src/Footer.js
@@ -3,6 +3,7 @@ function Footer(props){
     return (
         <>
         <p>Thank you Zixin for carrying the coding part</p>
+        <p>Thank you Alex for carrying the css part</p>
         <p>Group names: Oleksii Kravstov, Vincent Lau, Zixin Zou</p>        
         </>
         
